ff4d205ec38d320ba5197f8009d5ca09fc536097
Louie Alfonso <lfriasalfonso@gmail.com>
2025-02-18 23:57:39 +0000

    Create Category List Component & Angular Routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    FormsModule,
+    HttpClient,
+  ]
 };
---
+++ src/app/features/category/add-category/add-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-category',
+  imports: [FormsModule],
+  templateUrl: './add-category.component.html',
+  styleUrl: './add-category.component.css'
+})
+export class AddCategoryComponent {
+
+}
