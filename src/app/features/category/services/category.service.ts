f90c3fedd80144c52fdcd19cb5dafbb7b1961eb8
Louie Alfonso <lfriasalfonso@gmail.com>
2025-02-19 22:59:14 +0000

    Create UI Service Method & Display All Categories in Component

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.models';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
+import { Category } from '../models/category.models';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,14 @@ export class CategoryService {
   // add constructor
   constructor(private http: HttpClient) { }
 
-  // add addCategory
+  // add category
   addCategory(model : AddCategoryRequest) : Observable<void>{
-    return this.http.post<void>(`https://localhost:7027/api/v1/categories`, model);
+    return this.http.post<void>(`${environment.apiBaseUrl}/categories`, model);
+  }
+
+  // get all categories
+  getAllCategories(): Observable<Category[]>{
+     return this.http.get<Category[]>(`${environment.apiBaseUrl}/categories`);
   }
  
 }
