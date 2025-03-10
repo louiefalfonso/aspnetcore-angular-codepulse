a8b5359c313f80f9806520ecedf5bd72497b474a
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-10 18:39:37 +0000

    Create Home Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { EditCategoryComponent } from './features/category/edit-category/edit-ca
 import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
 import { EditBlogpostComponent } from './features/blog-post/edit-blogpost/edit-blogpost.component';
+import { HomeComponent } from './features/public/home/home.component';
 
 export const routes: Routes = [
     {
@@ -30,5 +31,9 @@ export const routes: Routes = [
     {
         path:"admin/blogposts/:id",
         component: EditBlogpostComponent
+    },
+    {
+        path:"",
+        component: HomeComponent
     },
 ];
